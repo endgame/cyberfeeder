778cf07d16e4b3fd21496007e03780112a3244fa
Jack Kelly <jack@jackkelly.name>
2013-05-02 20:30:35 +1000

    Load cards from a given data dir.

--- src/cyberfeeder.c
+++ src/cyberfeeder.c
@@ -20,19 +20,68 @@
 #endif
 
 #include <gtk/gtk.h>
+#include <stdlib.h>
 
 #include "card_db.h"
 #include "card_set.h"
 #include "pages.h"
 
 struct card_db *DB = NULL;
+gchar *DATA_DIR = NULL;
 
-int main(int argc, char *argv[]) {
-  gtk_init(&argc, &argv);
+static void read_db(void) {
+  gchar *card_dir = g_strconcat(DATA_DIR, G_DIR_SEPARATOR_S, "cards", NULL);
+  // TODO: Error checking!
+  GDir *dir = g_dir_open(card_dir, 0, NULL);
+  g_assert(dir != NULL);
+
+  GPtrArray /* of gchar* */ *files = g_ptr_array_new_with_free_func(g_free);
+  for (const gchar *file = g_dir_read_name(dir);
+       file != NULL;
+       file = g_dir_read_name(dir)) {
+    if (g_str_has_suffix(file, ".json")) {
+      g_ptr_array_add(files, g_strdup(file));
+    }
+  }
+  g_dir_close(dir);
+
+  gint cmp(gconstpointer a, gconstpointer b) {
+    return strcmp(*(gchar**)a, *(gchar**)b);
+  }
+  g_ptr_array_sort(files, cmp);
 
   DB = card_db_new();
-  card_db_add_set(DB, card_set_load_file("data/00_Core.json"));
-  card_db_add_set(DB, card_set_load_file("data/01_What_Lies_Ahead.json"));
+  for (guint i = 0; i < files->len; i++) {
+    gchar *file = g_strconcat(card_dir,
+                              G_DIR_SEPARATOR_S,
+                              g_ptr_array_index(files, i),
+                              NULL);
+    // TODO: Error checking!
+    struct card_set *set = card_set_load_file(file);
+    g_assert(set != NULL);
+    card_db_add_set(DB, set);
+    g_free(file);
+  }
+  g_ptr_array_free(files, TRUE);
+  g_free(card_dir);
+}
+
+int main(int argc, char *argv[]) {
+  GOptionEntry options[] = {
+    { "data-dir", 0, 0, G_OPTION_ARG_STRING, &DATA_DIR,
+      "Location of data files", "DIR" },
+    { NULL, 0, 0, G_OPTION_ARG_NONE, NULL, NULL, NULL }
+  };
+
+  gboolean ok = gtk_init_with_args(&argc, &argv, NULL, options, NULL, NULL);
+  if (!ok) {
+    fprintf(stderr, "Failed to initialise GTK.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (DATA_DIR == NULL) DATA_DIR = g_strdup(DEFAULT_DATA_DIR);
+
+  read_db();
 
   GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(window), "Cyberfeeder");
@@ -47,5 +96,7 @@ int main(int argc, char *argv[]) {
   gtk_main();
   card_db_free(DB);
   DB = NULL;
+  g_free(DATA_DIR);
+  DATA_DIR = NULL;
   return 0;
 }
